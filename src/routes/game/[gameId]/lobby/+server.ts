8d50d51a4efae74d1dd0321ab947aa98676390e7
alecbakholdin <alecbakholdin@gmail.com>
2023-09-07 14:22:58 -0400

    removed log statements

--- src/routes/game/[gameId]/lobby/+server.ts
+++ src/routes/game/[gameId]/lobby/+server.ts
@@ -2,8 +2,6 @@ import { json } from '@sveltejs/kit';
 
 export async function POST({locals}) {
     const {gameState} = locals;
-    console.log(gameState);
     gameState.state = gameState.state === 'lobby' ? 'inProgress' : 'lobby';
-    console.log(gameState);
     return json({message: 'success'});
 }
