f3ef02b42caeb77b2295ca9ea0483ff3fe22c9aa
alecbakholdin <alecbakholdin@gmail.com>
2023-09-10 16:11:05 -0400

    Added player indicator to GamePlayersList

--- src/routes/game/[gameId]/game/GamePlayerList.svelte
+++ src/routes/game/[gameId]/game/GamePlayerList.svelte
@@ -1,31 +1,46 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-import Resource from '$lib/components/Resource.svelte';
+	import Resource from '$lib/components/Resource.svelte';
 	import { raceInfoMap } from '$lib/types/race';
 	import { toSortedResourceArr } from '$lib/types/resource';
 	import { getGameContext } from '$lib/util/client/gameContext';
 
 	const gameContext = getGameContext();
 	const gameState = gameContext.gameState;
+	const onlineMembers = gameContext.onlineMembers;
 </script>
 
 <h4 class="h4">Players</h4>
 
 <div class="flex flex-col overflow-y-scroll gap-2">
-    {#each $gameState.players as playerId (playerId)}
-        <div class="card variant-soft flex flex-col border" style:border-color={raceInfoMap[$gameState.gameInfo[playerId].race].color}>
-            <header class="card-header flex flex-col">
-                <h6 class="h6">{$gameState.usernameMap[playerId]}</h6>
-                <span class="text-xs text-gray-400">{$gameState.gameInfo[playerId].race}</span>
-            </header>
-            <section class="p-4 flex-grow flex">
-                {#each toSortedResourceArr($gameState.gameInfo[playerId].resources) as resourceAmount}
-                    <Resource {...resourceAmount} />
-                {/each}
-                {#if $gameState.gameInfo[playerId].resources.length === 0}
-                    <span class="text-gray-400">No resources</span>
-                {/if}
-            </section>
-        </div>
-    {/each}
+	{#each $gameState.players as playerId (playerId)}
+		<div
+			class="card variant-soft flex flex-col border"
+			style:border-color={raceInfoMap[$gameState.gameInfo[playerId].race].color}
+		>
+			<header class="card-header flex flex-col">
+				<div class="flex items-center justify-between gap-2">
+					<h6 class="h6">{$gameState.usernameMap[playerId]}</h6>
+					{#if gameContext.me.id === playerId}
+						<iconify-icon icon="material-symbols:person" />
+					{:else}
+						<div
+							class="rounded-full w-2 h-2 border"
+							class:bg-gray-400={!$onlineMembers.find(({ id }) => id === playerId)}
+							class:bg-success-400={$onlineMembers.find(({ id }) => id === playerId)}
+						/>
+					{/if}
+				</div>
+				<span class="text-xs text-gray-400">{$gameState.gameInfo[playerId].race}</span>
+			</header>
+			<section class="p-4 flex-grow flex">
+				{#each toSortedResourceArr($gameState.gameInfo[playerId].resources) as resourceAmount}
+					<Resource {...resourceAmount} />
+				{/each}
+				{#if $gameState.gameInfo[playerId].resources.length === 0}
+					<span class="text-gray-400">No resources</span>
+				{/if}
+			</section>
+		</div>
+	{/each}
 </div>
